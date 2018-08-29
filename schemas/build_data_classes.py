db0ca3dc79d22a65229280532373a66e3d7b2d56
xamgore <xamgore@ya.ru>
2018-08-29 20:27:06 +0300

    Use typed schema instead of json loaded data in util scripts

--- schemas/build_data_classes.py
+++ schemas/build_data_classes.py
@@ -1,127 +1,88 @@
-from importlib import import_module
-from pathlib import Path
+from copy import copy
+from typing import Set, Dict
 
 from jinja2 import Template
 
-from schemas import structure
+from common import run_once
+from schemas.schema import load_schemas, Model, Property, Schema
 
 
-# template = Template('Hello {{ name }}!')
-# print(template.render(name='John Doe'))
-
-
-def rename_properties(model):
-    attrs = model['properties']
-    to_rename = {n: prop for n, prop in attrs.items() if 'rename' in prop}
+def rename_properties(model: Model):
+    to_rename: Set[Property] = {p for p in model.properties.values() if p.rename}
     remove_list = lambda ty: ty[5:-1] if ty.startswith('List[') and ty.endswith(']') else ty
 
-    for name, prop in to_rename.items():
-        # need to know, what property is used in rest api
-        prop['from'] = name
+    for prop in to_rename:
+        # need to know, what property's name is used in rest api
+        prop.from_name = prop.name
 
         # rename "lesson" to "lesson_id"
-        attrs[prop['rename']] = prop.copy()
-        del attrs[name]
+        model.properties[prop.rename] = copy(prop)
+        del model.properties[prop.name]
 
         # extract the inner type, so it could be imported
-        prop['type'] = remove_list(prop['new_type'])
+        # also used in the template, like ResourcesList[type]
+        prop.type = remove_list(prop.new_type)
 
         # make "lesson" either method, or list-property
-        new_prop_name = 'resources' if prop['new_type'].startswith('List[') else 'methods'
-        model[new_prop_name][name] = prop
+        if prop.new_type.startswith('List['):
+            model.resources[prop.name] = prop
+        else:
+            model.methods[prop.name] = prop
 
 
-def import_typings(model):
+@run_once
+def models() -> Dict[str, Model]:
+    """Return dict of all models"""
+    return {m.id: m for schema in load_schemas() for m in schema.models.values()}
+
+
+def get_imports_to_types(schema: Schema):
     """Search for types, used in properties & methods, and get correct imports"""
-    import_paths = structure.imports()
+    boxes = [box for m in schema.models.values()
+             for box in (m.properties, m.resources, m.methods)]
 
-    types = set().union(
-        {p['type'] for p in model['properties'].values()},
-        {p['type'] for p in model['resources'].values()},
-        {p['type'] for p in model['methods'].values()})
+    models_to_import = {prop.type for box in boxes for prop in box.values()} \
+        .intersection(models().keys())
 
-    return {type: import_paths[type] for type in types.intersection(import_paths.keys())}
+    return {m: f'api.{models()[m].schema.py_module_name}' for m in models_to_import}
 
 
-def refine_type(p):
-    """Get a python type by name of type in schema"""
-    ty = p['type']
+def improve_doc_string(p: Property):
+    """Surround the default value with commas if type equals to string"""
+    if p.format == 'string':
+        p.defaultValue = f'"{p.defaultValue}"'
 
-    if ty.startswith('Optional['):
-        return ty
-    if ty == 'integer':
-        return 'int'
-    if ty == 'boolean':
-        return 'bool'
-    if ty == 'decimal':
-        return 'float'
-    if ('description' in p) and (p['description'] == 'Enter a valid JSON object'):
-        p['description'] = None
-        return 'dict' if (p['defaultValue'] == '{}') else 'List'
-    if ty == 'dict':
-        return ty
-    if ty == 'string':
-        return 'str'
-    if ty.startswith('List['):
-        return ty
+    """Add information about the default value"""
+    p.docstring = p.description or ''
+    if p.defaultValue is not None:
+        p.docstring = f'{p.docstring}\n\nDefault value: ``{p.defaultValue}``'.lstrip()
 
     # todo: make smart types, like in structure.types()
-    if ty:
-        p['docstring'] = f"{p['docstring'] or ''}\n\nType: {ty}".lstrip('\n')
-
-    return 'str'
-
-
-def refine_doc_string_with_default_value(p):
-    """Add information about the default value"""
-    description = p.get('description', '') or ''
-    p['docstring'] = f'{description}\n\nDefault value: ``{p["defaultValue"]}``'.lstrip()
-
-
-def expand_with_meta_data(model_name, model):
-    model_name = model_name.replace('Serializer', '')
-
-    if Path(f'docs/{model_name}.py').exists():
-        extension = import_module(f'docs.{model_name}', package=None)
-
-        for prop, attrs in model['properties'].items():
-            if prop in dir(extension):
-                getattr(extension, prop)(attrs)
-
-
-def refine_default_value(p):
-    """Surround the default value with commas if type equals string"""
-    if ('format' in p) and p['format'] == 'string':
-        p['defaultValue'] = f'"{p["defaultValue"]}"'
+    if p.type not in ['string', 'integer', 'decimal', 'boolean']:
+        p.docstring = f"{p.docstring}\n\nType: {p.python_type}".lstrip('\n')
 
 
 if __name__ == '__main__':
     resources_without_models = []
 
-    for schema in structure.schemas():
-        if len(schema['models'].keys()) == 0:
-            resources_without_models.append(schema['resourcePath'])
+    for schema in load_schemas():
+        if len(schema.models) == 0:
+            resources_without_models.append(schema.resourcePath)
             continue
 
-        for name, model in schema['models'].items():
-            model['resources'], model['methods'] = {}, {}
+        for model in schema.models.values():
+            model.resources, model.methods = {}, {}
             rename_properties(model)
 
-            for pn, p in model['properties'].items():
-                p['docstring'] = p.get('description', None)
-                p['type'] = refine_type(p)
-
-                if 'defaultValue' in p:
-                    refine_default_value(p)
-                    refine_doc_string_with_default_value(p)
-
-            model['imports'] = import_typings(model)
+            for pn, p in model.properties.items():
+                improve_doc_string(p)
 
         with open('python-templates/data-class.jinja2') as t:
-            output_module = schema["resourcePath"].replace('-', '_')
-
-            with open(f'../{output_module}.py', 'w') as out:
+            with open(f'../api/{schema.py_module_name}.py', 'w') as out:
                 template = Template(t.read(), lstrip_blocks=True, trim_blocks=True)
-                out.write(template.render(models=schema['models']))
+                out.write(template.render(
+                    models=schema.models,
+                    imports=get_imports_to_types(schema))
+                )
 
     print(f'{len(resources_without_models)} resources without models:', *resources_without_models, sep='\n  ')
